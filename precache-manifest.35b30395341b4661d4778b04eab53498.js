20554caa9fb5875168a692cf17795d64b2a43f39
Ilya Mityaev <ilyamityaev@yahoo.com>
2022-04-25 22:25:58 +0300

    mains

---
+++ precache-manifest.35b30395341b4661d4778b04eab53498.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dea5b3d2b5db439ee75f",
+    "url": "/solid-stats/css/app.4cd6f013.css"
+  },
+  {
+    "revision": "68313b086dc6f69356ab",
+    "url": "/solid-stats/css/chunk-vendors.24ce4a6c.css"
+  },
+  {
+    "revision": "04e04b39c1493715e37e1bf9ff00083b",
+    "url": "/solid-stats/index.html"
+  },
+  {
+    "revision": "dea5b3d2b5db439ee75f",
+    "url": "/solid-stats/js/app.fd116e15.js"
+  },
+  {
+    "revision": "3146cf36851557e5c440",
+    "url": "/solid-stats/js/chunk-2d0c42b2.f2d09a50.js"
+  },
+  {
+    "revision": "d49eabf5a52830dfee21",
+    "url": "/solid-stats/js/chunk-2d21a3d2.64b41c77.js"
+  },
+  {
+    "revision": "7c10fae1aae60f4b40c7",
+    "url": "/solid-stats/js/chunk-2d21b308.b13da508.js"
+  },
+  {
+    "revision": "68313b086dc6f69356ab",
+    "url": "/solid-stats/js/chunk-vendors.0e5c7a08.js"
+  },
+  {
+    "revision": "a512c48fd2481172a12b211442f57904",
+    "url": "/solid-stats/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/solid-stats/robots.txt"
+  },
+  {
+    "revision": "74f642327f8313bb93e08239f83e8a5e",
+    "url": "/solid-stats/stats.json"
+  }
+]);
